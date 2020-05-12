27a9b88830ade42539fd963bfe1d2d316c1eb53f
billowycloud <bjkim0228@naver.com>
2020-05-12 15:09:07 +0900

    make it better on heroku

---
+++ frontend/build/precache-manifest.a406544dcf024dcbf361109c24c67e8b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a37355c64b78d12b5bc084ba26bdc14a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9ca4d7fc6be2154319c",
+    "url": "/static/js/2.67f96ee4.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/static/js/2.67f96ee4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8eb95e1dff5dded20dd",
+    "url": "/static/js/main.01a09888.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  },
+  {
+    "revision": "1a6abc37967de62138e8a9573f7a6609",
+    "url": "/static/media/mainBackground.1a6abc37.jpg"
+  },
+  {
+    "revision": "12e01d355ddd10c40fbdfd126dd8d81a",
+    "url": "/static/media/noBackdrop.12e01d35.png"
+  },
+  {
+    "revision": "b82adbcddc7038f9de220ff07b713650",
+    "url": "/static/media/noPoster.b82adbcd.png"
+  },
+  {
+    "revision": "a24fffb9796d518e22282e9955cfadbc",
+    "url": "/static/media/profile.a24fffb9.png"
+  }
+]);
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-require('dotenv').config(); //.env 파일에서 환경 변수 불러오기
-import Koa from 'koa'; //const Koa = require("koa");
-import Router from 'koa-router';
-import api from './api';
-import jwtMiddleware from './lib/jwtMiddleware';
-import bodyParser from 'koa-bodyparser';
-import mongoose from 'mongoose';
+require("dotenv").config(); //.env 파일에서 환경 변수 불러오기
+import Koa from "koa"; //const Koa = require("koa");
+import Router from "koa-router";
+import api from "./api";
+import jwtMiddleware from "./lib/jwtMiddleware";
+import bodyParser from "koa-bodyparser";
+import mongoose from "mongoose";
+import serve from "koa-static";
+import path from "path";
+import send from "koa-send";
 
 const { PORT, MONGO_URI } = process.env;
 const app = new Koa();
@@ -18,23 +21,33 @@ mongoose
     useCreateIndex: true,
   })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log("Connected to MongoDB");
   })
   .catch((e) => {
     console.log(`DB Connection Error: ${e.message}`);
   });
 
 //라우터 설정
-router.use('/api', api.routes());
+router.use("/api", api.routes());
+
+const buildDirectory = path.resolve(__dirname, "../frontend/build");
 
 app
   .use(bodyParser()) //bodyParser 는 라우터 전에 적용하기
   .use(jwtMiddleware)
   .use(router.routes()) //라우터 적용
-  .use(router.allowedMethods());
+  .use(router.allowedMethods())
+  .use(serve(buildDirectory))
+  .use(async (ctx) => {
+    // Not Found 이고, 주소가 /api 로 시작하지 않는 경우
+    if (ctx.status === 404 && ctx.path.indexOf("/api") !== 0) {
+      // index.html 내용을 반환
+      await send(ctx, "index.html", { root: buildDirectory });
+    }
+  });
 
 //포트 미지정시 4000 사용
 const port = PORT || 4000;
 app.listen(port, () => {
-  console.log('Listening to port: %d', port);
+  console.log("Listening to port: %d", port);
 });
